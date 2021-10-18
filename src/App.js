e00f501a7eef18d556232f92e406d8eb75dd8ccb
Benedict-Khamana <benkama@hotmail.be>
2021-10-18 11:07:35 +0200

    updating app to redirect to home if sign-in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -8,7 +8,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpComponent from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { /*getDoc, */ onSnapshot } from '@firebase/firestore';
+import { onSnapshot } from '@firebase/firestore';
 import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
@@ -23,39 +23,19 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(
-      async userAuth => {
-        if (userAuth) {
-          const userRef = await createUserProfileDocument(userAuth);
-          /*const unsubscribe = */
-          onSnapshot(userRef, snapShot => {
-            setCurrentUser(
-              {
-                // currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              }
-              /* }*/
-            );
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        /*const unsubscribe = */
+        onSnapshot(userRef, snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
-          // if (docSnap.exists()) {
-          // console.log('Document data:', docSnap.data());
-          //   this.props.setCurrentUser({
-          //     currentUser: {
-          //       id: docSnap.id,
-          //       ...docSnap.data(),
-          //     },
-          //   });
-          // } else {
-          // doc.data() will be undefined in this case
-          // console.log('No such document!');
-          // this.setState({ currentUser: userAuth });
-        } /*else {*/
-        setCurrentUser(userAuth);
+        });
       }
-      // }
-      /*}*/
-    );
+      setCurrentUser(userAuth);
+    });
   }
 
   componentWillUnmount() {
@@ -68,13 +48,28 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpComponent} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpComponent />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)),
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
