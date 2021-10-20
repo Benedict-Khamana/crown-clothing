e5b94cfbf77d0b031cf2537651edc0fd73526bdf
Benedict-Khamana <benkama@hotmail.be>
2021-10-20 08:56:19 +0200

    Get collection data from firebase and convert for the app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -90,6 +90,20 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
+export const convertCollectionSnapshopToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+  console.log(transformedCollection);
+};
+
 const firebaseApp = initializeApp(firebaseConfig);
 
 export const auth = getAuth(firebaseApp);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,13 +2,37 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import { collection, onSnapshot } from '@firebase/firestore';
+import {
+  firestore,
+  convertCollectionSnapshopToMap,
+} from '../../firebase/firebase.utils';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = collection(firestore, 'collections');
+
+    onSnapshot(collectionRef, async snapshot => {
+      convertCollectionSnapshopToMap(snapshot);
+    });
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = {};
 
 export default ShopPage;
